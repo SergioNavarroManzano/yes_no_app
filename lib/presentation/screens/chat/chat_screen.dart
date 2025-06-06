3656e4cc7008771e21ca201a5dca0e5fca92ef2e
Sergio Navarro Manzano <sergionavarroman@gmail.com>
2025-06-06 08:54:06 +0200

    Commit con parte interfaz

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+              'https://i.pinimg.com/736x/fd/7e/ac/fd7eac51d4a09870d23e33ff95fa6418.jpg',
+            ),
+          ),
+        ),
+        title: Text('Mi Bichito ❤️'),
+        centerTitle: true,
+        backgroundColor: colors.primaryContainer,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return (index % 2 == 0)
+                      ? const HerMessageBubble()
+                      : const MyMessageBubble();
+                },
+              ),
+            ),
+            MessageFieldBox()
+          ],
+        ),
+      ),
+    );
+  }
+}
